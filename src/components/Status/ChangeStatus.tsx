39ff68cb8489ac9ace60c2c72f6ef8318c45cf8f
Thomsleg <Thomsleg@hotmail.com>
2022-05-26 21:47:14 +0200

    Update UserStatus

--- src/components/Status/ChangeStatus.tsx
+++ src/components/Status/ChangeStatus.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import { User } from '../../types';
+import UserService from '../../services/UserService';
 import "./status.css";
 import axios from 'axios';
 
 const Status: React.FC = () => {
-    const [status, setStatus] = useState<string>('');
-    const changeStatusUser = async ( status: string) => {
-       try {
-           const res = await axios.put('http://localhost:3000/user/status', {status: status});
-           window.location.reload();
-           return {status: 'success'};
-       } catch (error: any) {
-           return error;
-       }
-    }
+  const token = sessionStorage.getItem('token');
+  const JSONtoken = JSON.parse((token) ? token : '{}');
+  const UserID = (JSONtoken) ? JSONtoken["user"]["user_id"] : null;
+  const [status, setStatus] = useState<string>('');
+  const changeStatusUser = async ( status: string) => {
+    try {
+      const res = await UserService.changeStatus(status, UserID);
+      window.location.reload();
+      return {status: 'success'};
+    } catch (error: any) {
+        return error;
+      }
+  }
 
     const handleStatusSubmit = async (e: { preventDefault: () => void; }) => {
         e.preventDefault();
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,7 +2,8 @@ import axios from '../axios';
 import { User } from '../types';
 
 const getAllUsers = () => axios.get<Array<User>>('/users');
+const changeStatus = (status: string, UserID: number) => axios.put<User>('/user/status', { status: status, UserID : UserID});
 
-const UserService = { getAllUsers,};
+const UserService = { getAllUsers, changeStatus};
 
 export default UserService;
