159c93676a87367f4841b7823265f2642200575d
Louis Schaaf <50657206+Louisschaaf@users.noreply.github.com>
2022-05-26 12:26:29 +0200

    fixed login

---
+++ src/services/MessageService.ts
@@ -0,0 +1,13 @@
+/*import axios from '../axios';
+import { User } from '../types';
+
+const getAllMessages = (UserID: number) => axios.get<Array<User>>('/friends', { params: { UserID } });
+
+const addFriend = (friendID: number) => axios.post<User>('/friends', { friendID });
+
+const FriendsService = {
+    getAllFriends,
+    addFriend,
+};*/
+
+export default null;
